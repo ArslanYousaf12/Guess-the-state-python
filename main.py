f14d5908884fddc0992dc99f9a0bb59ba5e301ec
Arslan Yousaf <devtechofficial90@gmail.com>
2025-04-08 11:29:13 +0500

    Refactor code to remove unused CSV reading logic and switch to pandas for data handling

--- main.py
+++ main.py
@@ -3,16 +3,21 @@
 #     print(data)
 
 
-import csv
+# import csv
 
-with open("weather_data.csv") as file_data:
-    data = csv.reader(file_data)
-    temp = []
-    for row in data:
-        if row[1] == "temp":
-            continue
-        print(row[1])
-        temperature = int(row[1])
-        temp.append(temperature)
+# with open("weather_data.csv") as file_data:
+#     data = csv.reader(file_data)
+#     temp = []
+#     for row in data:
+#         if row[1] != "temp":
+#             # print(row[1])
+#             temperature = int(row[1])
+#             temp.append(temperature)
         
-print(temp)
+# print(temp)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(data)
+print(data["temp"])
