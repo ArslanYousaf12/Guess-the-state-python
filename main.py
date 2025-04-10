b5be18c4df34235e52f873dc0669ca9ec8812bd4
Arslan Yousaf <devtechofficial90@gmail.com>
2025-04-10 11:00:59 +0500

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -18,6 +18,72 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
+# Data frames are like table in excel
+# Series are like column
+# print(type(data))
 # print(data)
-print(data["temp"])
+# # print(type(data["temp"]))
+# data_dict = data.to_dict()
+# print(data_dict) 
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+# average_temp = round( sum(temp_list) / len(temp_list))
+# print(average_temp)
+# total_temps = 0
+# count = 0
+# for temp in temp_list:
+#     count = count + 1
+#     total_temps = total_temps + temp
+
+# print(total_temps)
+# print(count)
+
+# average_temp = int(total_temps / count)
+# print(average_temp)
+
+# average = data['temp'].mean()
+# print(average)
+
+# max = data["temp"].max()
+# print(max)
+
+# print(data.condition)
+
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+# monday = data[data.day == "Monday"]
+# temp_degree = monday.temp
+# temp_farh = temp_degree * 9/5 + 32
+# print(temp_farh)
+
+# data_dict = {
+#     "students": ["Amy", "james", "Angela"],
+#     "Scores": [76,56,65]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")
+
+
+data = pandas.read_csv("weather_data.csv")
+data_dict = data.to_dict()
+temp_list = data.temp.to_list
+average = data.temp.mean()
+max_temp = data.temp.max()
+monday = data[data.day == "Monday"]
+temp_max = data[data.temp == data.temp.max()]
+
+temp = monday.temp
+print(temp)
+
+
+student_dict = {
+    "students": ["Arslan", "kamran", "Ali"],
+    "Scores": [56,77,34],
+}
+
+data_frame = pandas.DataFrame(student_dict)
+print(data_frame)
+data_frame.to_csv("Abc.csv")
