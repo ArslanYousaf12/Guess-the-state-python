96eefbaf7b33002b9465eb50a0ba7c2c516deacc
Arslan Yousaf <devtechofficial90@gmail.com>
2025-04-12 17:38:21 +0500

    Refactor game logic to streamline state guessing process and improve user interaction

--- main.py
+++ main.py
@@ -23,31 +23,21 @@ import pandas
 
 data = pandas.read_csv("50_states.csv")
 
-TOTAL_STATES = 50
+all_states = data.state.to_list()
+guess_states = []
 
-correct_answer= 0
-while correct_answer < TOTAL_STATES:
-    screen.title(f"{correct_answer}/50 US-State-Game")
-    turtle.home()
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name? ").title()
+while len(guess_states) <= 50: 
+    answer_state = screen.textinput(title=f"{len(guess_states)}/50 correct", prompt="What's another state's name? ").title()
 
     print(answer_state)
-    
-    state = data[data.state == answer_state]
-    state_name = state.state
-    print(state_name)
-    if(state_name.empty):
-        continue
-    else:
-        correct_answer += 1 
-        state_xcor = float(state.x)
-        state_ycor = float(state.y)
-        turtle.penup()
-        turtle.setposition(x=state_xcor, y=state_ycor)
-        turtle.write(answer_state)
-    
-   
+    if answer_state in all_states:
+        guess_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        print(state_data)
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
-
-
-turtle.mainloop()
+screen.exitonclick()
